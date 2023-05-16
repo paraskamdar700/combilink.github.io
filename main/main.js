1b26f75efe3af075f73598e6e0b33c060306fbac
Nuralcode797 <133733635+Nuralcode797@users.noreply.github.com>
2023-05-16 10:23:30 +0530

    Delete main directory

--- main/main.js
+++
@@ -1,9 +0,0 @@
-let introTXT= document.getElementById("introTXT");
-let delay = 100;
-
-for (const ele of introTXT.innerHTML) {
-    setTimeout(() => {
-        introTXT.innerHTML += ele;
-    }, delay);
-    delay += 100;
-}
